e34a28db930e6e8aa036f3566073ed1c874b386a
Jacob Klinger <jacobklinger1@me.com>
2016-10-05 08:30:08 -0400

    Add study abroad cards and fix nav bugs

--- script.js
+++ script.js
@@ -40,54 +40,68 @@ function showAllCards(except) {
     $("div.extracurricular").show();
 }
 
+function clearNavHighlights() {
+    $("div.nav-link a.edu").attr("currentNav", "false");
+    $("div.nav-link a.work").attr("currentNav", "false");
+    $("div.nav-link a.skills").attr("currentNav", "false");
+    $("div.nav-link a.extracurricular").attr("currentNav", "false");
+}
+
 
 $(document).ready(function(){
     $("header h1 a").click(function() {
         showAllCards();
+        clearNavHighlights();
         currentNav = null;
     });
+    
+    /*
+        On click functions
+        
+        for each navigation link:
+            when clicked:
+                if not already the current filter:
+                    hide all cards except category clicked
+                    set currentnav attribute to true (so navigation is highlighted)
+                if already the current filter:
+                    show all cards
+    */
     
     $("div.nav-link a.edu").click(function() {
         if(currentNav != "education") {
             hideCardsExcept("education");
             currentNav = "education";
-        }
-        else {
-            showAllCards();
-            currentNav = null;
-        }     
+            clearNavHighlights();
+            $("div.nav-link a.edu").attr("currentNav", "true");
+        }   
     });
     
     $("div.nav-link a.work").click(function() {
         if(currentNav != "work") {
             hideCardsExcept("work");
             currentNav = "work";
+            clearNavHighlights();
+            $("div.nav-link a.work").attr("currentNav", "true");
         }
-        else {
-            showAllCards();
-            currentNav = null;
-        } 
     });
     
     $("div.nav-link a.skills").click(function() {
         if(currentNav != "skills") {
             hideCardsExcept("skills");
             currentNav = "skills";
+            clearNavHighlights();
+            $("div.nav-link a.skills").attr("currentNav", "true");
         }
-        else {
-            showAllCards();
-            currentNav = null;
-        } 
     });
     
     $("div.nav-link a.extracurricular").click(function() {
         if(currentNav != "extracurricular") {
             hideCardsExcept("extracurricular");
             currentNav = "extracurricular";
+            clearNavHighlights();
+            $("div.nav-link a.extracurricular").attr("currentNav", "true");
         }
-        else {
-            showAllCards;
-            currentNav = null;
-        } 
     });
 });
+
+
